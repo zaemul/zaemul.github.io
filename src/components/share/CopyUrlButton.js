8a7ec5a9bfdc4c33827ccdb0fb9efe83652e781c
joseph song <joseph.song@kakaoenterprise.com>
2021-02-21 01:56:45 +0900

    복사 버그 수정

--- src/components/share/CopyUrlButton.js
+++ src/components/share/CopyUrlButton.js
@@ -12,16 +12,22 @@ const imgStyle = css({
 });
 
 const CopyUrlButton = ({ url }) => {
-  const clipboard = useClipboard();
-
-  const handleCopy = useCallback(
-    (url) => {
+  const clipboard = useClipboard({
+    onSuccess() {
       Analytics.sendEvent({
         category: 'Share',
         action: 'Copy URL',
         label: 'Share button',
       });
-      alert('링크가 복사되었습니다!');
+      alert('주소가 복사되었습니다');
+    },
+    onError() {
+      alert('복사에 실패하였습니다');
+    },
+  });
+
+  const handleCopy = useCallback(
+    (url) => {
       clipboard.copy(url);
     },
     [clipboard.copy, url]
